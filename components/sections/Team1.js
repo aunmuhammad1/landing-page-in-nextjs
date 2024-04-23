16e561253affffcf58a528c454cea3bbe8c146db
Aun Muhammad <92050402+aunmuhammad6307@users.noreply.github.com>
2024-04-24 00:32:32 +0500

    section 8 set

--- components/sections/Team1.js
+++ components/sections/Team1.js
@@ -18,45 +18,54 @@ export default function Team1() {
                             <h2 className="text-white wow fadeInUp" data-wow-delay=".3s">Trinzz is Enterprise-Ready</h2>
                         </div>
                     </div>
-                    <div className="team-wrapper">
-                        <div className="team-items bor-top bor-bottom wow fadeInUp" data-wow-delay=".3s">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center'
+                    }} >
+                        <div className="team-wrapper">
+                            <div className="team-items bor-top wow fadeInUp" data-wow-delay=".3s">
+                                <i class="fa fa-check-square" aria-hidden="true"></i>
+                                <p>
+                                Leverage Trinzz capabilities for accuracy, efficiency and speed, Securely and Scalably
+                                </p>
+                            </div>
+                            <div className="team-items pt-0 wow fadeInUp" data-wow-delay=".5s">
+                                <i class="fa fa-check-square" aria-hidden="true"></i>
+                                <p>
+                                Compliant with SOC2 Type 2 standards
+                                </p>
+                            </div>
+                            <div className="team-items pt-0 wow fadeInUp" data-wow-delay=".7s">
+                                <i class="fa fa-check-square" aria-hidden="true"></i>
+                                <p>
+                                Encryption of all data in transit and at rest ensuring end-to-end security, with SSL
+    transport rated A+ by Qualys
+                                </p>
+                            </div>
+                            <div className="team-items pt-0 wow fadeInUp" data-wow-delay=".9s">
+                                <i class="fa fa-check-square" aria-hidden="true"></i>
+                                <p>
+                                Benefit from enterprise-grade infrastructure and regulatory compliance
+                                </p>
+                            </div>
+                            <div className="team-items pt-0 wow fadeInUp" data-wow-delay=".9s">
                             <i class="fa fa-check-square" aria-hidden="true"></i>
-                            <p>
-                            Leverage Trinzz capabilities for accuracy, efficiency and speed, Securely and Scalably
-                            </p>
-                        </div>
-                        <div className="team-items bor-bottom pt-0 wow fadeInUp" data-wow-delay=".5s">
+                                <p>
+                                Ready for scale through Google Cloud Platform and Amazon Web Services
+    hosting
+                                </p>
+                            </div>
+                            <div className="team-items pt-0 wow fadeInUp" data-wow-delay=".9s">
                             <i class="fa fa-check-square" aria-hidden="true"></i>
-                            <p>
-                            Compliant with SOC2 Type 2 standards
-                            </p>
+                                <p>
+                                Infrastructure compliant with HIPAA, enabling Business Associate Agreements
+    execution
+                                </p>
+                            </div>
                         </div>
-                        <div className="team-items bor-bottom pt-0 wow fadeInUp" data-wow-delay=".7s">
-                            <i class="fa fa-check-square" aria-hidden="true"></i>
-                            <p>
-                            Encryption of all data in transit and at rest ensuring end-to-end security, with SSL
-transport rated A+ by Qualys
-                            </p>
-                        </div>
-                        <div className="team-items bor-bottom pt-0 wow fadeInUp" data-wow-delay=".9s">
-                            <i class="fa fa-check-square" aria-hidden="true"></i>
-                            <p>
-                            Benefit from enterprise-grade infrastructure and regulatory compliance
-                            </p>
-                        </div>
-                        <div className="team-items bor-bottom pt-0 wow fadeInUp" data-wow-delay=".9s">
-                        <i class="fa fa-check-square" aria-hidden="true"></i>
-                            <p>
-                            Ready for scale through Google Cloud Platform and Amazon Web Services
-hosting
-                            </p>
-                        </div>
-                        <div className="team-items bor-bottom pt-0 wow fadeInUp" data-wow-delay=".9s">
-                        <i class="fa fa-check-square" aria-hidden="true"></i>
-                            <p>
-                            Infrastructure compliant with HIPAA, enabling Business Associate Agreements
-execution
-                            </p>
+                        <div>
+                            <img src="https://assets-global.website-files.com/5f6bc60e665f54545a1e52a5/645eb70d91a5004db42ae2c7_rf_enterprise_gradient2-p-500.webp" />
                         </div>
                     </div>
                 </div>
